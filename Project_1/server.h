8197ef30ee57b3826eaa8fd222d611ee16f9184c
Brad Morgan <jbradmorg16@gmail.com>
2024-03-12 07:37:56 -0400

    first commit

---
+++ Project_1/server.h
@@ -0,0 +1,27 @@
+#ifndef Server_h
+#define Server_h
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+
+struct Server {
+    int domain;
+    int service;
+    int protocol;
+    u_long interface;
+    int port;
+    int backlog;
+
+    struct sockaddr_in address;
+
+    int socket;
+
+    void (*launch)(void);
+};
+
+struct Server server_constructor(int domain, int service, int protocol, u_long interface, int port, int backlog, void(*launch)(void));
+
+#endif
